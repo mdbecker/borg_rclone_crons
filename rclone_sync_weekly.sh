4eb43db1ff426c4562e4e239dbd3c9155201008e
Michael Becker <mdbecker@users.noreply.github.com>
2020-12-20 23:59:46 -0500

    Update rclone_sync_weekly.sh

--- rclone_sync_weekly.sh
+++ rclone_sync_weekly.sh
@@ -2,4 +2,4 @@
 dt=`date +%Y-%m-%d-%H_%M_%S`
 # This one makes one request per file not efficient but more accurate. Run this weekly.
 /usr/bin/time /usr/bin/rclone sync -vv --exclude "/logs/" --dump headers --fast-list --checksum --log-file=/logdir/sync-log-weekly-${dt}.log --transfers 2 --s3-chunk-size 16M /mnt/borgdirectory remote:bucket-name > /logdir/sync-time-weekly-${dt}.out 2>&1
-/usr/bin/mv /logdir/sync-* /mnt/borgdirectory/logs
+/usr/bin/find /logdir -name "sync-*" -type f -size +0 -printf "/usr/bin/mv '%p' /mnt/borgdirectory/logs;" | bash
